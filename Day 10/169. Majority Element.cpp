416d048288e5706f095c2c4037b93d51855ec005
Chinmay Kulkarni <79635843+chinmaykulkarni19@users.noreply.github.com>
2022-01-14 19:48:01 +0530

    Add files via upload

---
+++ Day 10/169. Majority Element.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        int res=0;
+        int count=1;
+        for(int i=1;i<nums.size();i++)
+        {
+            if(nums[res]==nums[i])
+                count++;
+            else
+                count--;
+            if(count==0)
+                res=i,count=1;
+        }
+        count=0;
+        for(int i=0;i<nums.size();i++)
+        {
+            if(nums[res]==nums[i])
+                count++;
+            
+               
+                
+        }
+        if(count <= nums.size()/2)
+                res=-1;
+        return nums[res];
+    }
+};
