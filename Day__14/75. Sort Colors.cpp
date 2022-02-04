873dabb2f40991eb8a0ae29d6ff8851263575ce3
Chinmay Kulkarni <79635843+chinmaykulkarni19@users.noreply.github.com>
2022-02-04 11:20:10 +0530

    Add files via upload

---
+++ Day__14/75. Sort Colors.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    void sortColors(vector<int>& nums) {
+        int l=0,mid=0,h=nums.size()-1;
+        while(mid<=h)
+        {
+            if(nums[mid]==0)
+            {
+                swap(nums[mid],nums[l]);
+                mid++;
+                l++;
+            }
+            else if(nums[mid]==1)
+                mid++;
+            else
+            {
+                swap(nums[mid],nums[h]);
+                h--;
+            }
+                
+        }
+    }
+};
