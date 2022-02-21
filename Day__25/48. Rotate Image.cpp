cafe1f4adb574cf3f970e23253f6ad4e80718371
Chinmay Kulkarni <79635843+chinmaykulkarni19@users.noreply.github.com>
2022-02-21 10:13:57 +0530

    Add files via upload

---
+++ Day__25/48. Rotate Image.cpp
@@ -0,0 +1,28 @@
+class Solution
+{
+public:
+    void rotate(vector<vector<int>> &matrix)
+    {
+        int n = matrix.size();
+
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = i + 1; j < n; j++)
+            {
+                swap(matrix[i][j], matrix[j][i]);
+            }
+        }
+
+        for (int i = 0; i < n; i++)
+        {
+            int left = 0;
+            int right = n - 1;
+            while (left < right)
+            {
+                swap(matrix[i][left], matrix[i][right]);
+                left++;
+                right--;
+            }
+        }
+    }
+};
