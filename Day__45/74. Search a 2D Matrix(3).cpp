4f0254a7fd08d703a167a0da358a56678f39f864
Chinmay Kulkarni <79635843+chinmaykulkarni19@users.noreply.github.com>
2022-03-20 01:04:27 +0530

    Add files via upload

---
+++ Day__45/74. Search a 2D Matrix(3).cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        int row=0,col=matrix[0].size()-1;
+        while(row<matrix.size() && col>=0)
+        {
+            if(matrix[row][col]==target)
+                return true;
+            if(matrix[row][col]>target)
+                col--;
+            else
+                row++;
+        }
+        return 0;
+    }
+};
