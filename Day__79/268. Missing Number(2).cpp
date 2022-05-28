839424627b3ba00d4ea177dc340bd41754385542
Chinmay Kulkarni <79635843+chinmaykulkarni19@users.noreply.github.com>
2022-05-28 23:05:00 +0530

    Add files via upload

---
+++ Day__79/268. Missing Number(2).cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
+        int total=(nums.size()*(nums.size()+1))/2;
+        int sum=0;
+        for(auto num: nums)
+        {
+            sum+=num;
+        }
+        return total-sum;
+    }
+};
