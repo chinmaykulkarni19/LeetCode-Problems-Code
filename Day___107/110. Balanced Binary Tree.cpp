c6bdea06a2c39dcc92d3092ff6e3a913f6e38a3e
Chinmay Kulkarni <79635843+chinmaykulkarni19@users.noreply.github.com>
2022-07-02 22:27:38 +0530

    Add files via upload

---
+++ Day___107/110. Balanced Binary Tree.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool isBalanced(TreeNode* root) {
+        if(root==NULL)
+            return true;
+        int lh=height(root->left);
+        int rh=height(root->right);
+        return (abs(lh-rh)<=1 && isBalanced(root->left) && isBalanced(root->right));
+    }
+    int height(TreeNode *root)
+    {
+        if(root==NULL)
+            return 0;
+        return 1+max(height(root->left),height(root->right));
+    }
+};
