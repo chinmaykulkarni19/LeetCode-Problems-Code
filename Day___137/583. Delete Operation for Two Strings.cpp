8e49d8a4f8df91fe0f88d476f4e3f95c7d544303
Chinmay Kulkarni <chinmay.22010250@viit.ac.in>
2022-08-16 17:10:14 +0530

    583. Delete Operation for Two Strings

---
+++ Day___137/583. Delete Operation for Two Strings.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    int minDistance(string word1, string word2) {
+        int m=word1.size();
+        int n=word2.size();
+        int dp[m+1][n+1];
+        for(int i=0;i<m+1;i++)
+        {
+            for(int j=0;j<n+1;j++)
+            {
+                if(i==0||j==0)
+                    dp[i][j]=0;
+            }
+        }
+        for(int i=1;i<m+1;i++)
+        {
+            for(int j=1;j<n+1;j++)
+            {
+                if(word1[i-1]==word2[j-1])
+                {
+                    dp[i][j]=1+dp[i-1][j-1];
+                }
+                else
+                {
+                    dp[i][j]=max(dp[i-1][j],dp[i][j-1]);
+                }
+            }
+        }
+        return m+n-(2*dp[m][n]);
+    }
+};
