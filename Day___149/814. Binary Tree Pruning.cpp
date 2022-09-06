be697b4c577287f67632f0ea9fcc8238acd1a89d
Chinmay Kulkarni <chinmay.22010250@viit.ac.in>
2022-09-06 23:29:42 +0530

    814. Binary Tree Pruning

---
+++ Day___149/814. Binary Tree Pruning.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    TreeNode* pruneTree(TreeNode* root) {
+        if(root!=NULL)
+        {
+            root->left=pruneTree(root->left);
+            root->right=pruneTree(root->right);
+            if(root->left==NULL && root->right==NULL && root->val==0)
+                return NULL;
+        }
+        return root;
+    }
+};
