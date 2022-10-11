a882c4a17303a0412dd1a5a6392380fd3882b6e9
Chinmay Kulkarni <chinmay.22010250@viit.ac.in>
2022-10-11 23:41:12 +0530

    334. Increasing Triplet Subsequence

---
+++ Day___171/334. Increasing Triplet Subsequence.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool increasingTriplet(vector<int>& nums) {
+        int first=INT_MAX;
+        int second=INT_MAX;
+        for(int i=0;i<nums.size();i++)
+        {
+            if(nums[i]<first)
+            {
+                first=nums[i];
+            }
+            else if(first<nums[i] && nums[i]<second)
+                second=nums[i];
+            else if(second<nums[i])
+                return true;
+        }
+        return false;
+    }
+};
