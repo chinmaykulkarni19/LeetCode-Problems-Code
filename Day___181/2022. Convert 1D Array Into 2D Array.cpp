a2a409a0a430e4802ce2cc3c124c2bebbaf241bb
Chinmay Kulkarni <chinmay.22010250@viit.ac.in>
2022-10-23 13:59:36 +0530

    645. Set Mismatch

---
+++ Day___181/2022. Convert 1D Array Into 2D Array.cpp
@@ -0,0 +1,10 @@
+vector<vector<int>> construct2DArray(vector<int>& original, int m, int n) {
+        if(original.size()!=m*n)
+            return {};
+        vector<vector<int>>res(m,vector<int>(n));
+        for(int i=0;i<original.size();i++)
+        {
+            res[i/n][i%n]=original[i];
+        }
+        return res;
+    }
