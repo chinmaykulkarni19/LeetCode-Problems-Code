ff94b2a47e051224c7acc4f7d900b15fa894dedb
Chinmay Kulkarni <79635843+chinmaykulkarni19@users.noreply.github.com>
2022-10-24 17:06:17 +0530

    Add files via upload

---
+++ Day___182/451. Sort Characters By Frequency.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    string frequencySort(string s) {
+        unordered_map<char,int>res;
+        for(auto it:s)
+            res[it]++;
+        priority_queue<pair<int,char>>pq;
+        for(auto it:res)
+            pq.push({it.second,it.first});
+        string ans="";
+        while(!pq.empty())
+        {
+            ans+=string(pq.top().first,pq.top().second);
+            pq.pop();
+        }
+        return ans;
+    }
+};
